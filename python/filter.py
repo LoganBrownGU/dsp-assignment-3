bade8cca333f3d1e4f7b9cf9f8690c9bcc297fa7
LoganBrownGU <2641407b@student.gla.ac.uk>
2024-12-03 14:07:20 +0000

    Added working IIR filter

--- python/filter.py
+++ python/filter.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Nov 28 08:34:21 2024
+Created on Mon Dec  2 09:23:31 2024
 
 @author: Dell
 """
@@ -16,10 +16,10 @@ def SecondOrder ( fs, f1, f2 ):
     fc = ( f1 + f2 ) / 2
     w0 = np.pi * 2 * fc / fs
     r = 0.9
-   
+  
     a = [ 1, -2 * r * np.cos( w0 ), r ** 2 ]
     b = [ 1, -2 * np.cos( w0 ), 1 ]
-       
+      
     return b, a
 
 
@@ -27,69 +27,59 @@ def Butter( fs, f1, f2 ):
     nyquist = fs / 2
     lowCutoff = f1 / nyquist
     highCutoff = f2 / nyquist
-   
+  
     b, a = butter( 1, [ lowCutoff, highCutoff ], btype = 'bandstop' )
-   
+  
     return b, a
-
-
-def doFiltering( data ):
-    buffer1 = [0, 0]
-    buffer2 = [0, 0]
-    inputacc = np.zeros( len( data ) )
-    outputacc = np.zeros( len ( data ) )
-   
-    b, a = Butter( 1000, 45, 55 )
-   
-    for n in range( len( data ) ):
-       
-        # accumulator for the IIR part
-        inputacc[n] =  data[n]
-        inputacc[n] = inputacc[n] - ( a[1] * buffer1[0] )
-        inputacc[n] = inputacc[n] - ( a[2] * buffer2[0] )
-       
-        # accumulator for the FIR part
-        outputacc[n] = inputacc[n] + b[0]
-        outputacc[n] = outputacc[n] + ( b[1] * buffer1[1] )
-        outputacc[n] = outputacc[n] + ( b[2] * buffer2[1] )
-
-        buffer1.pop(0)
-        buffer2.pop(0)
-        buffer1.append(inputacc[n])
-        buffer2.append(outputacc[n])
-       
-    return outputacc
+      
 
 class Filter():
-    def __init__(self, fs, fc1, fc2):
+   def __init__(self, fs, fc1, fc2):
         self.b, self.a = SecondOrder(fs, fc1, fc2)
         print(self.a)
         print(self.b)
-        self.buf1 = 0
-        self.buf2 = 0
+       
+       # normalise the coefficients
+        self.b = [ b_i / self.a[0] for b_i in self.b ]
+        self.a = [ a_i / self.a[0] for a_i in self.a ]
+       
+        self.buf1_in = 0
+        self.buf2_in = 0
+        self.buf1_out = 0
+        self.buf2_out = 0
 
-    def filter(self, sample):
-        input_acc  = sample - self.a[0] * self.buf1 - self.a[1] * self.buf2 
-        output_acc = input_acc * self.b[0] + self.buf1 * self.b[1] + self.b[2] * self.buf2
+   def filter(self, sample):
+       # apply direct form i filter
+        input_acc  = sample - self.a[1] * self.buf1_in - self.a[2] * self.buf2_in
+        output_acc = input_acc * self.b[0] + self.buf1_in * self.b[1] + self.b[2] * self.buf2_in
 
-        self.buf2 = self.buf1
-        self.buf1 = input_acc
+       # update buffers
+        self.buf2_in = self.buf1_in
+        self.buf1_in = input_acc
+        self.buf2_out = self.buf1_out
+        self.buf1_out = output_acc
 
         return output_acc
 
 # print( butter( 4, [ 0.1, 0.2 ], btype = 'bandstop', output="sos" ))
 
-t = np.linspace(0, 1, 1000)
+# test signal
+fs = 1000
+t = np.linspace(0, 1, fs)
 data = np.sin(5 * 2 * np.pi * t) + np.sin(50 * 2 * np.pi * t) + np.sin(100 * 2 * np.pi * t)
 out_data = []
 
-filter = Filter(1000, 45, 55)
+# apply the filter
+out_data = []
+filter = Filter(fs, 45, 55)
 for d in data:
     out_data.append(filter.filter(d))
 
-b, a = SecondOrder(1000, 45, 55)
-w, h = signal.freqz(b, a)
-plt.plot(w / np.pi / 2, np.abs(h))
-# plt.plot(np.abs(np.fft.fft(data)))
-# plt.plot(np.abs(np.fft.fft(out_data)))
+plt.plot(np.abs(np.fft.fft(data)) / len(data), label= 'Original FFT')
+plt.plot(np.abs(np.fft.fft(out_data) / len(out_data)), label= 'Filtered FFT')
+plt.xlabel("Frequency (Hz)")
+plt.ylabel("Amplitude")
+plt.title("FFT of Original and Filtered Signals")
+plt.legend()
+plt.grid()
 plt.show()
