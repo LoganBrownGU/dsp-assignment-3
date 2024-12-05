dc3f29c0d00a1f5108d0adad7db6350fc7c25e50
LoganBrownGU <2641407b@student.gla.ac.uk>
2024-12-05 16:30:43 +0000

    Real time graph and better filter

--- python/filter.py
+++ python/filter.py
@@ -21,8 +21,8 @@ def highpass(fs, fc):
  
    return b, a
 
-class Filter():
-   def __init__(self, fs, fc1 ):
+class IIRFilter():
+    def __init__(self, fs, fc1 ):
        self.b, self.a = bandpass(fs, fc1 - 2, fc1 + 2)
        
        # normalise the coefficients
@@ -34,7 +34,7 @@ class Filter():
        self.buf1_out = 0
        self.buf2_out = 0
 
-   def filter(self, sample):
+    def filter(self, sample):
        # apply direct form i filter
        input_acc  = sample - self.a[1] * self.buf1_in - self.a[2] * self.buf2_in
        output_acc = input_acc * self.b[0] + self.buf1_in * self.b[1] + self.b[2] * self.buf2_in
@@ -47,11 +47,21 @@ class Filter():
 
        return output_acc
 
+class Filter():
+    def __init__(self, fs, fc1):
+        self.__iirs = [IIRFilter(fs, fc1) for i in range(3)]
+
+    def filter(self, sample):
+        for iir in self.__iirs:
+           sample = iir.filter(sample)
+
+        return sample
+
 def test_filter():
     # test signal
     fs = 1000
     t = np.linspace(0, 1, fs)
-    data = np.sin(5 * 2 * np.pi * t) + np.sin(50 * 2 * np.pi * t) + np.sin(100 * 2 * np.pi * t)
+    data = np.sin(5 * 2 * np.pi * t) + np.sin(50 * 2 * np.pi * t) + np.sin(70 * 2 * np.pi * t)
     out_data = []
 
     # apply the filter
@@ -68,3 +78,5 @@ def test_filter():
     plt.legend()
     plt.grid()
     plt.show()
+
+# test_filter()
--- python/receiver.py
+++ python/receiver.py
@@ -8,6 +8,43 @@ import matplotlib.pyplot as plt
 from pyfirmata2 import Arduino
 from threading import Timer
 
+import matplotlib
+matplotlib.use('Qt5Agg')
+from PyQt6 import QtCore, QtWidgets
+from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+import pyqtgraph as pg # tested with pyqtgraph==0.13.7
+
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+import pyqtgraph as pg
+
+samples = [[], []]
+
+class MainWindow(QtWidgets.QMainWindow):
+        def __init__(self):
+            super(MainWindow, self).__init__()
+
+            self.time_plot = pg.PlotWidget()
+            self.time_plot.setYRange(0, 0.1)
+            self.time_plot_curve1 = self.time_plot.plot([])
+            self.time_plot_curve2 = self.time_plot.plot([])
+            self.setCentralWidget(self.time_plot)
+
+            # Setup a timer to trigger the redraw by calling update_plot.
+            self.timer = QtCore.QTimer()
+            self.timer.setInterval(0) # causes the timer to start immediately
+            self.timer.timeout.connect(self.update_plot) # causes the timer to start itself again automatically
+            self.timer.start()
+            self.start_t = time.time() # used for benchmarking
+
+            self.show()
+
+        def update_plot(self):
+            global samples
+            self.time_plot_curve1.setData(samples[0][-2000:])
+            self.time_plot_curve2.setData(samples[1][-2000:])
+
+
 class Receiver():
     def __init__(self, baud, sampling_rate, board, analogue_channel, f1, f2):
         self.uart = uart.UART_Rx(baud, self.end)
@@ -30,7 +67,10 @@ class Receiver():
         a1 = np.max(self.buf1)
         a2 = np.max(self.buf2)
 
-        thresh = 0.03
+        samples[0].append(a1)
+        samples[1].append(a2)
+
+        thresh = 0.02
         self.uart.d = 0 if a2 < thresh else 1
 
         # print(f"{"\b" * 100}: {a1}", end="")
@@ -46,3 +86,7 @@ f1 = 20
 f2 = 40
 receiver = Receiver(baud, 500, board, 1, f1, f2)
 time.sleep(1)
+
+app = QtWidgets.QApplication([])
+w = MainWindow()
+app.exec()
