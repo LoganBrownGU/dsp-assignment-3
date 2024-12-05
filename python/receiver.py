fe5084d20c9fc6957fdf9efbb707728240b37206
LoganBrownGU <2641407b@student.gla.ac.uk>
2024-12-05 14:53:14 +0000

    Separated tx and rx closes #2

--- python/receiver.py
+++ python/receiver.py
@@ -8,44 +8,9 @@ import matplotlib.pyplot as plt
 from pyfirmata2 import Arduino
 from threading import Timer
 
-class Transmitter():
-    def __init__(self, baud, board, f1, f2):
-        self.uart = uart.UART_Tx(baud, None)
-        self.board = board
-        self.output_pin = self.board.get_pin("d:4:o")
-        self.output_pin.write(1)
-
-        self.state = False
-        self.lo_timer = Timer(0.5/f2, self.lo)
-        self.lo_timer.start()
-
-        self.f1 = f1
-        self.f2 = f2
-
-    def lo(self):
-        self.lo_timer = Timer(0.5 / (self.f2 if self.uart.q == 1 else self.f1), self.lo)
-        self.lo_timer.start()
-
-        self.state = not self.state
-        self.output_pin.write(self.state)
-
-    def start(self, data):
-        
-        self.uart.send_bulk(data, self.end)
-
-    def end(self):
-        chr_str = "".join([f"{chr(x)}" for x in out_bytes])
-        print(f"{chr_str}")
-
-out_bytes = []
-
-def foo():
-    out_bytes.append(receiver.uart.get_buf())
-    print(chr(out_bytes[-1]), end="", flush=True)
-
 class Receiver():
     def __init__(self, baud, sampling_rate, board, analogue_channel, f1, f2):
-        self.uart = uart.UART_Rx(baud, foo)
+        self.uart = uart.UART_Rx(baud, self.end)
         self.board = board
         self.board.samplingOn(1000 / sampling_rate)
         self.board.analog[analogue_channel].register_callback(self.update)
@@ -68,18 +33,16 @@ class Receiver():
         thresh = 0.03
         self.uart.d = 0 if a2 < thresh else 1
 
+        # print(f"{"\b" * 100}: {a1}", end="")
+
+    def end(self):
+        print(chr(receiver.uart.get_buf()), end="", flush=True)
+
 PORT = Arduino.AUTODETECT
 board = Arduino(PORT,debug=True)
 
 baud = 10
 f1 = 20
 f2 = 40
-transmitter = Transmitter(baud, board, f1, f2)
-time.sleep(1)
 receiver = Receiver(baud, 500, board, 1, f1, f2)
 time.sleep(1)
-string = "" 
-with open("assets/to_send.txt") as f: 
-    string = f.read()
-print("".join([f"{x:08b}" for x in map(ord, string)]))
-transmitter.start(map(ord, string))
--- python/transmitter.py
+++ python/transmitter.py
@@ -1,40 +1,47 @@
 import uart
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 from pyfirmata2 import Arduino
 from threading import Timer
 
 class Transmitter():
+    def __init__(self, baud, board, f1, f2):
+        self.uart = uart.UART_Tx(baud, None)
+        self.board = board
+        self.output_pin = self.board.get_pin("d:4:o")
+        self.output_pin.write(1)
 
-    def __init__(self, clock_freq, time_step, data, board):
-        self.uart = uart.UART_Tx(clock_freq, time_step)
-        self.uart.start(data.pop(0))
-        self.data = data
-        self.output_pin = board.get_pin("d:4:o")
-        self.timer = None
-        self.time_step = time_step
-    
+        self.state = False
+        self.lo_timer = Timer(0.5/f2, self.lo)
+        self.lo_timer.start()
 
-    def callback(self):
-        self.timer = Timer(self.time_step, self.callback)
-        self.timer.start()
+        self.f1 = f1
+        self.f2 = f2
 
-        if self.uart.finished:
-            self.uart.start(self.data.pop(0))
-            
-        v = self.uart.send()
-        self.output_pin.write(v)
+    def lo(self):
+        self.lo_timer = Timer(0.5 / (self.f2 if self.uart.q == 1 else self.f1), self.lo)
+        self.lo_timer.start()
 
-    def start(self):
-        self.timer = Timer(self.time_step, self.callback)
-        self.timer.start()
+        self.state = not self.state
+        self.output_pin.write(self.state)
+
+    def start(self, data):
+        self.uart.send_bulk(data, self.end)
+
+    def end(self):
+        time.sleep(1)
+        print("done")
+        self.lo_timer.cancel()
+        self.board.exit()
 
 PORT = Arduino.AUTODETECT
 board = Arduino(PORT,debug=True)
 
-data = [[0, 1, 0, 1, 0, 1, 0, 1] for i in range(10)]
-
-print(data)
-tx = Transmitter(1, 0.001, data, board)
-tx.start()
+transmitter = Transmitter(10, board, 20, 40) 
+string = "" 
+with open("assets/to_send.txt") as f: 
+    string = f.read()
+print("".join([f"{x:08b}" for x in map(ord, string)]))
+transmitter.start(map(ord, string))
