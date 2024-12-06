e0c8d9b703f9cde63cc37ade9031fa01a6b20253
LoganBrownGU <2641407b@student.gla.ac.uk>
2024-12-06 13:39:51 +0000

    Better plotting

---
+++ python/graph.py
@@ -0,0 +1,39 @@
+import matplotlib
+matplotlib.use('Qt5Agg')
+from PyQt6 import QtCore, QtWidgets
+from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+import pyqtgraph as pg # tested with pyqtgraph==0.13.7
+
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+import pyqtgraph as pg
+
+class Graph(QMainWindow):
+    def __init__(self, n_plots, back_samples):
+        super(Graph, self).__init__()
+
+        self.back_samples = back_samples
+
+        self.time_plot = pg.PlotWidget()
+        self.time_plot.setYRange(0, 0.1)
+        # self.time_plot_curve1 = self.time_plot.plot([], symbolPen=pg.mkPen(color=(200, 200, 255)))
+        # self.time_plot_curve2 = self.time_plot.plot([])
+        self.setCentralWidget(self.time_plot)
+
+            # Setup a timer to trigger the redraw by calling update_plot.
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(0) # causes the timer to start immediately
+        self.timer.timeout.connect(self.update_plot) # causes the timer to start itself again automatically
+        self.timer.start()
+
+        self.plot_curves = [self.time_plot.plot([]) for _ in range(n_plots)]
+        self.samples = [[] for _ in range(n_plots)]
+
+        self.show()
+
+    def update_plot(self):
+        for plot_curve, samples in zip(self.plot_curves, self.samples):
+            plot_curve.setData(samples[-self.back_samples:])
+
+    def add_sample(self, sample, plot):
+        self.samples[plot].append(sample)
--- python/receiver.py
+++ python/receiver.py
@@ -5,44 +5,15 @@ import time
 import numpy as np
 import scipy.signal as signal
 import matplotlib.pyplot as plt
+import matplotlib
 from pyfirmata2 import Arduino
 from threading import Timer
+from graph import Graph
 
-import matplotlib
 matplotlib.use('Qt5Agg')
-from PyQt6 import QtCore, QtWidgets
-from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.figure import Figure
-import pyqtgraph as pg # tested with pyqtgraph==0.13.7
-
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
-import pyqtgraph as pg
+from PyQt6 import QtWidgets
 
 samples = [[], []]
-
-class MainWindow(QtWidgets.QMainWindow):
-        def __init__(self):
-            super(MainWindow, self).__init__()
-
-            self.time_plot = pg.PlotWidget()
-            self.time_plot.setYRange(0, 0.1)
-            self.time_plot_curve1 = self.time_plot.plot([])
-            self.time_plot_curve2 = self.time_plot.plot([])
-            self.setCentralWidget(self.time_plot)
-
-            # Setup a timer to trigger the redraw by calling update_plot.
-            self.timer = QtCore.QTimer()
-            self.timer.setInterval(0) # causes the timer to start immediately
-            self.timer.timeout.connect(self.update_plot) # causes the timer to start itself again automatically
-            self.timer.start()
-            self.start_t = time.time() # used for benchmarking
-
-            self.show()
-
-        def update_plot(self):
-            global samples
-            self.time_plot_curve1.setData(samples[0][-2000:])
-            self.time_plot_curve2.setData(samples[1][-2000:])
 
 
 class Receiver():
@@ -73,17 +44,17 @@ class Receiver():
         self.__uart.d = 0 if a2 < thresh else 1
 
     def end(self):
-        print(chr(receiver.__uart.get_buf()), end="", flush=True)
+        print(chr(self.__uart.get_buf()), end="", flush=True)
 
-PORT = Arduino.AUTODETECT
-board = Arduino(PORT,debug=True)
+# PORT = Arduino.AUTODETECT
+# board = Arduino(PORT,debug=True)
 
-baud = 10
-f1 = 20
-f2 = 40
-receiver = Receiver(baud, 500, board, 1, f1, f2)
-time.sleep(1)
+# baud = 10
+# f1 = 20
+# f2 = 40
+# receiver = Receiver(baud, 500, board, 1, f1, f2)
+# time.sleep(1)
 
 app = QtWidgets.QApplication([])
-w = MainWindow()
+w = Graph()
 app.exec()
